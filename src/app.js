99c4d5f4087da525e0032b10599a8f26ea32e63f
edoka <peter.edoka.242464@unn.edu.ng>
2023-02-16 03:04:13 +0100

    added mongoose connection

--- src/app.js
+++ src/app.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const CONNECTION = process.env.CONNECTION
 
 app.get('/', (req, res) => {
     // console.log(req.method + ' ' + req.url);
     res.send('Hello world!');
 });
 
-app.listen(PORT, () => {
-    console.log('Server running on port ' + PORT);
-})
+
+const start = async() => {
+    try {
+        await mongoose.connect(CONNECTION);
+        
+        app.listen(PORT, () => {
+            console.log('Server running on port ' + PORT);
+        });
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+start();
