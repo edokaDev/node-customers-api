cf631f7ffe58a713afd16cb237f659b5e38e3c97
edoka <peter.edoka.242464@unn.edu.ng>
2023-02-16 05:03:09 +0100

    added customer get and create endpoints

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Customer = require('./models/customer');
 
 mongoose.set('strictQuery', false);
 
@@ -11,12 +12,35 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const CONNECTION = process.env.CONNECTION
 
+// This is so that we can use the req.body
+// to get data from post request
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ENDPOINTS
 app.get('/', (req, res) => {
     // console.log(req.method + ' ' + req.url);
     res.send('Hello world!');
 });
 
+app.get('/api/customers', async (req, res) => {
+    console.log(req.body);
+    const customers = await Customer.find();
+    res.json({customers});
+});
 
+app.post('/api/customers', async (req, res) => {
+    // console.log(req);
+    const customer = new Customer(req.body);
+    try {
+        await customer.save();
+        res.status(201).json({customer});
+    } catch (e) {
+        res.status(400).json({msg: e.message})
+    }
+});
+
+// DB Connection
 const start = async() => {
     try {
         await mongoose.connect(CONNECTION);
@@ -29,4 +53,5 @@ const start = async() => {
     }
 }
 
+// Init
 start();
